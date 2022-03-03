11df3b3f5a0ab89978e53fd6d0ac7511dabd0809
Max Zampieri <98056019+MaxZampieri@users.noreply.github.com>
2022-03-02 23:20:57 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const c = (el)=> {
+    return document.querySelector(el);
+}
+const cs = (el)=> {
+    return document.querySelectorAll(el);
+}
+
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true)
+    
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        c('.pizzaBig img').src = pizzaJson[key].img;
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
+
+    c('.pizza-area').append( pizzaItem );
+});
