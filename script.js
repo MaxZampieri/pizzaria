74207d721f6d4871c61ddef33c6ca40f7501b2d2
Max Zampieri <98056019+MaxZampieri@users.noreply.github.com>
2022-03-03 16:23:24 -0300

    Início de implementação javascript

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let modalQt = 1;
+let cart = [];
+
 const c = (el)=> {
     return document.querySelector(el);
 }
@@ -5,9 +8,9 @@ const cs = (el)=> {
     return document.querySelectorAll(el);
 }
 
-
+// Listagem das pizzas
 pizzaJson.map((item, index)=>{
-    let pizzaItem = c('.models .pizza-item').cloneNode(true)
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
     
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
@@ -17,10 +20,21 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
-
+        modalQt = 1;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaBig img').src = pizzaJson[key].img;
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2) {
+                size.classList.add('selected');
+            }
+
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
@@ -31,3 +45,33 @@ pizzaJson.map((item, index)=>{
 
     c('.pizza-area').append( pizzaItem );
 });
+// Eventos do modal
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none'
+    }, 500);
+
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal)
+})
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1){
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+})
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+})
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    })
+});
+c('.pizza--addButton').addEventListener('click', ()=>{
+
+})
